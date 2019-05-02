4c03db68fb9cfb001cd29778db9fb4ff6c14fa9a
Ednilson Luciano Cipolla <ednilson.cipolla@gmail.com>
2019-05-02 02:00:22 -0300

    A

---
+++ CadClientes/CadClientes/Models/EstadoDAO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadClientes.Models
+{
+    public class EstadoDAO
+    {
+    }
+}
--- CadClientes/CadClientes/Models/EstadoService.cs
+++ CadClientes/CadClientes/Models/EstadoService.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace CadClientes.Models.Interfaces
+namespace CadClientes.Models
 {
-    public class iEstado
+    public class EstadoService
     {
     }
 }
